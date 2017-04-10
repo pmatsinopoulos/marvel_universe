76073fef8a7c43f8feb8d646c178e541e7291241
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-04-10 08:38:05 +0300

    Make sure that you set the correct active class to the menu items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def top_menu_active(menu_item_text)
+    if controller_name == 'public' && action_name == 'index'
+      if menu_item_text == 'Home'
+        return 'active'
+      end
+    end
+
+    if controller_name == 'characters' && action_name == 'index'
+      if menu_item_text == 'Characters'
+        return 'active'
+      end
+    end
+  end
 end
