71b0ba2bf737b2e44ef6adca716533ec4e80c5b0
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-04-10 07:53:28 +0300

    root support

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'characters', to: 'characters#index'
+
+  root 'public#index'
 end
